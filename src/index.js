e2282380464fe2e595350ba8995821d525567f25
Loktysh Alexandr <vebster24@mail.ru>
2020-10-21 13:52:03 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+    return n = n.toString().split('').reverse().join('').replace(/^0+/, '').split('').map((b) => Number.isInteger(parseInt(b)) ? parseInt(b) : '').join('');
+    ;
 }
